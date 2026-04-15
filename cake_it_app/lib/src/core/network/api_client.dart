74e149180f35e888c49dcae30a368b13adaada89
Niza <ebenezerundie@gmail.com>
2026-04-15 20:25:05 +0100

    feat: add timeout to API requests

--- cake_it_app/lib/src/core/network/api_client.dart
+++ cake_it_app/lib/src/core/network/api_client.dart
@@ -9,7 +9,10 @@ class ApiClient {
   ApiClient({required this.baseUrl});
 
   Future<dynamic> get(String path) async {
-    final response = await http.get(Uri.parse('$baseUrl$path'));
+    final response = await http
+        .get(Uri.parse('$baseUrl$path'))
+        .timeout(const Duration(seconds: 20));
+    ;
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
